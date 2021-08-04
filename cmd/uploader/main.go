b2c511327d550c136e10bdb7bd81c5c8ce23e308
Alexandr <admin@plin2k.org>
2021-08-05 00:05:00 +0400

    one in one

--- cmd/uploader/main.go
+++ cmd/uploader/main.go
@@ -28,10 +28,30 @@ func upload(c *gin.Context) {
 	c.Redirect(http.StatusSeeOther, "/file/"+file.Filename)
 }
 
+func download(c *gin.Context) {
+	c.File("./storage/" + c.Param("filename"))
+}
+
+func file(c *gin.Context) {
+	c.HTML(http.StatusOK, "file.tmpl", gin.H{"filename": c.Param("filename")})
+}
+
 func main() {
 	router := gin.Default()
+
 	router.LoadHTMLGlob("web/*")
+
 	router.GET("/", index)
 	router.POST("/upload", upload)
+
+	router.GET("/download/:filename", download)
+	router.GET("/file/:filename", file)
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
 	router.Run(":9999")
 }
